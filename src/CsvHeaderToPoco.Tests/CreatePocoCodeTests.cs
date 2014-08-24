78a35cd1dd9c5bea0622f70894c8b5e324400030
Orcomp <Orcomp@server>
2014-08-24 21:56:16 +1000

    Initial Commit

---
+++ src/CsvHeaderToPoco.Tests/CreatePocoCodeTests.cs
@@ -0,0 +1,15 @@
+﻿namespace CsvHeaderToPoco.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CreatePocoCodeTests
+    {
+
+        [Test]
+        public void CreateFiles()
+        {
+            
+        }
+    }
+}
---
+++ src/CsvHeaderToPoco/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CsvHeaderToPoco
+{
+    using System;
+    using System.IO;
+
+    using System.Reflection;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var csvFolderPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var namespaceName = "Test.Entities";
+
+            Orc.Csv.CodeGeneration.CreateCSharpFilesForAllCsvFiles(csvFolderPath, namespaceName, Path.Combine(csvFolderPath, "Output"));
+            Console.ReadLine();
+        }
+    }
+}
