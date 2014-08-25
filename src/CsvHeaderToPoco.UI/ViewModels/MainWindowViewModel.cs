f4c0705b0d575f01db9d764d90710206d33cf54a
DanPristupov <danil@danil.cz>
2014-08-25 12:54:31 +0200

    Adds Catel ViewModel

---
+++ src/CsvHeaderToPoco.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Catel.MVVM;
+
+namespace CsvHeaderToPoco.UI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            
+        }
+    }
+}
